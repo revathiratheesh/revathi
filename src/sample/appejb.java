664dcb8bfd378eaae887f5846bbe4e3f4b351fab
mazenet1 <mazenet1@Mazenet-PC>
2014-02-12 15:46:20 +0530

    Signed-off-by: mazenet1 <mazenet1@Mazenet-PC>

---
+++ src/sample/appejb.java
@@ -0,0 +1,26 @@
+package sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.naming.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class appejb
+ */
+public class appejb extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
